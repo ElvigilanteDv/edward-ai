ac88cdfc0712724c4a83d3576caf33711eb4aea3
El vigilante <cololacalempira5@gmail.com>
2026-06-23 21:48:25 -0600

    Actualizar db.js

--- config/db.js
+++ config/db.js
@@ -1,12 +1,15 @@
 const mongoose = require('mongoose');
 
+// El enlace de tu base de datos fija
+const DB_URI = 'mongodb+srv://DvWilkerOFC:dvwilker15@dvwilker15.xndilqb.mongodb.net/wilker_api?retryWrites=true&w=majority';
+
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI);
+        const conn = await mongoose.connect(DB_URI);
         console.log(`MongoDB Conectado: ${conn.connection.host}`);
     } catch (error) {
         console.error(`Error de conexión: ${error.message}`);
-        process.exit(1); // Detiene la app si no se puede conectar
+        process.exit(1);
     }
 };
 
